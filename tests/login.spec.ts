9fc72e0de668ae065972a37246bd28fa5e6de03e
Marek Szumny <lukmarcus@gmail.com>
2023-05-16 14:55:36 +0200

    test cleaning

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,19 +3,15 @@ import { test, expect } from "@playwright/test";
 test.describe("Demobank user login page", () => {
   test("successful login with correct credentials", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("user1234");
-    await page.getByTestId("password-input").click();
     await page.getByTestId("password-input").fill("password");
     await page.getByTestId("login-button").click();
-    await page.getByTestId("user-name").click();
 
     await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
   });
 
   test("unsuccessful login with too short username", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").click();
     await page.getByTestId("login-input").fill("user");
     await page.getByTestId("login-input").blur();
 
@@ -26,7 +22,6 @@ test.describe("Demobank user login page", () => {
 
   test("unsuccessful login with too short password", async ({ page }) => {
     await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("password-input").click();
     await page.getByTestId("password-input").fill("pass");
     await page.getByTestId("password-input").blur();
 
