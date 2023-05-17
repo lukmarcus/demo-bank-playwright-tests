7475088448fec8f643021e684204621e8c6b6393
Marek Szumny <lukmarcus@gmail.com>
2023-05-17 14:15:39 +0200

    2nd desktop test

--- tests/desktop.spec.ts
+++ tests/desktop.spec.ts
@@ -17,4 +17,21 @@ test.describe("Desktop", () => {
       "Przelew wykonany! Chuck Demobankowy - 120,00PLN - Zwrot środków"
     );
   });
+
+  test("successfull mobile top-up", async ({ page }) => {
+    await page.goto("https://demo-bank.vercel.app/");
+    await page.getByTestId("login-input").fill("user1234");
+    await page.getByTestId("password-input").fill("password");
+    await page.getByTestId("login-button").click();
+
+    await page.locator("#widget_1_topup_receiver").selectOption("500 xxx xxx");
+    await page.locator("#widget_1_topup_amount").fill("50");
+    await page.locator("#uniform-widget_1_topup_agreement").check();
+    await page.locator("#execute_phone_btn").click();
+    await page.getByTestId("close-button").click();
+
+    await expect(page.getByTestId("message-text")).toHaveText(
+      "Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx"
+    );
+  });
 });
